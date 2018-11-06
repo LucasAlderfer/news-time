14201062813f41f5c3449aa01ed1bab54bc8a58c
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-11-06 15:57:32 -0700

    Changes requirements for favorites so articles without images or descriptions can still be favorited

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,9 +1,7 @@
 class Favorite < ApplicationRecord
   validates_presence_of :source_name,
                         :title,
-                        :description,
                         :url,
-                        :urltoimage,
                         :published_at
 
   belongs_to :user
--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -4,9 +4,7 @@ describe Favorite, type: :model do
   context 'validations' do
     it { should validate_presence_of(:source_name) }
     it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:description) }
     it { should validate_presence_of(:url) }
-    it { should validate_presence_of(:urltoimage) }
     it { should validate_presence_of(:published_at) }
   end
 
