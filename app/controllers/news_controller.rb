2b756ee4170aeae84e2a2a372f1060386c2b3115
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-11-06 14:01:14 -0700

    Updates styling for desired newspaper look

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,13 @@
 class NewsController < ApplicationController
 
   def index
-    if !params['sources'].nil?
-      search_params = "&sources=#{params['sources']}"
-    elsif !params['category'].nil?
-      search_params = "&category=#{params['category']}"
-    elsif !params['q'].nil?
-      search_params = "&q=#{params['q']}"
+    search_params = ""
+    if params['sources'] != "" && params['sources'] != nil
+      search_params += "&sources=#{params['sources']}"
+    elsif params['category'] != "" && params['category'] != nil
+      search_params += "&category=#{params['category']}"
+    elsif params['q'] != "" && params['q'] != nil
+      search_params += "&q=#{params['q']}"
     else
       search_params = nil
     end
